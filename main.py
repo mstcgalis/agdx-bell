a93d1d837526c7ba28b37fe82ac2898fd45d4d6d
Daniel Gális <danielgalis21@gmail.com>
2022-05-31 17:30:29 +0200

    bell class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 ####
 
 import os
+from time import time
 from playsound import playsound
 import datetime
 import json
@@ -15,9 +16,60 @@ sound_pause_end_path = os.path.normpath("sounds/wood.mp3")
 
 harmonogram_path = "data/harmonogram.json"
 
+## CONVERSION from markdown to harmonogram
+def md2harmonogram(inp):
+    inp = inp.replace("`"," ")
+    lines = inp.split('\n')
+    list=[]
+    keys=[]
+    day=""
+    date=""
+    when=""
+    year = "2022-"
+    type="block"
+    harmonogram = []
+    format = "%Y-%d.%m.%H:%M"
+
+    for i,line in enumerate(lines):
+        line = line.strip(" ")
+        if i == 0:
+            # from the first line in the table, get the day and dat by spliting at space
+            day, date = line.split('|')[1].strip().split(" ")
+        elif i == 2:
+            # the third line holds keys
+            keys=[_i.strip() for _i in line.split('|')]
+        else:
+            dict = ({keys[_i]:v.strip() for _i,v in enumerate(line.split('|')) if  _i>0 and _i<len(keys)-1})
+            if dict:
+                list.append(dict)
+    
+    # turn the table list of dicts to a harmonogram of intervals
+    for i, dict in enumerate(list):
+        when = dict.get("WHEN")
+        start = datetime.datetime.strptime(year+date+when.split("-")[0], format)
+        end = ""
+
+        what = dict.get("WHAT","")
+        if "pauza" in what or "pause" in what or "příchod" in what:
+            type = "pause"
+        else:
+            type = "block"
+
+        item = interval( start, end, type, dict.get("WHO",""), what, dict.get("TIME",""), dict.get("WHERE",""))
+        harmonogram.append(item)
+    
+    # add end_time to all items
+    for i, item in enumerate(harmonogram):
+        try:
+            item.end_time = harmonogram[i+1].start_time
+        except:
+            item.end_time = datetime.datetime.strptime(year+date+"00:00", format)
+
+    return harmonogram
+
 ## INTERVAL class
 class interval:
-    def __init__(self, date, start, end="", type="block", who="", title="", time="", where=""):
+    def __init__(self, start, end="", type="block", who="", title="", time="", where=""):
         
         self.start_time = start
 
@@ -36,48 +88,31 @@ class interval:
     def __str__(self):
         return f"{self.start_time} {self.type} {self.title}"
 
-## CONVERSION from markdown to harmonogram
-def md2harmonogram(inp):
-    inp = inp.replace("`"," ")
-    lines = inp.split('\n')
-    list=[]
-    keys=[]
-    day=""
-    date=""
-    when=""
-    year = "2022_"
-    type="block"
-    harmonogram = []
-    format = "%Y_%d.%m.%H:%M"
-
-    for i,line in enumerate(lines):
-        line = line.strip(" ")
-        if i == 0:
-            # from the first line in the table, get the day and dat by spliting at space
-            day, date = line.split('|')[1].strip().split(" ")
-        elif i == 2:
-            # the third line holds keys
-            keys=[_i.strip() for _i in line.split('|')]
-        else:
-            dict = ({keys[_i]:v.strip() for _i,v in enumerate(line.split('|')) if  _i>0 and _i<len(keys)-1})
-            if dict:
-                list.append(dict)
+## TIMER class
+class timer:
+    def __init__(self, harmonogram):
+        self.harmonogram = harmonogram
     
-    for dict in list:
-        when = dict.get("WHEN","")
-        start = datetime.datetime.strptime(year+date+when.split("-")[0], format)
-        try:
-            end = datetime.datetime.strptime(year+date+when.split("-")[1], format)
-        except:
-             end = ""
-        
-        what = dict.get("WHAT","")
-        if "pauza" in what or "pause" in what:
-            type = "pause"
-
-        item = interval(date, start, end, type, dict.get("WHO",""), what, dict.get("TIME",""), dict.get("WHERE",""))
-        harmonogram.append(item)
-    return harmonogram
+    def arm(self):
+        current_time = datetime.datetime.now()
+        for i, item in enumerate(self.harmonogram):
+            print(i, item)
+            while not item.done:
+                current_time = datetime.datetime.now()
+                # if start_time uz bol, ale nie davnejseie ako 30 sekund, tak zvon
+                if item.start_time <= current_time < item.start_time + datetime.timedelta(seconds=30):
+                    if item.type == "pause":
+                        playsound(sound_pause_start_path)
+                        item.done = True
+                    elif harmonogram[i-1].type == "pause":
+                        playsound(sound_pause_end_path)
+                        item.done = True
+                    else:
+                        playsound(sound_block_path)
+                        item.done = True
+                # ak start_time uz bol, davnejsie ako 1 minutu dozadu, tak je uz item done
+                elif item.start_time < current_time:
+                    item.done = True
 
 ## HARMONOGRAM functions
 def save_harmonogram(harmonogram, file_path):
@@ -90,21 +125,25 @@ def save_harmonogram(harmonogram, file_path):
 
 def load_harmonogram(file_path):
     harmonogram = []
-
+    format = "%Y-%m-%d %H:%M:%S"
     with open(file_path) as f:
         try: list = json.load(f)
         except: return False
-    
     for dict in list:
-        harmonogram.append(interval(dict.get("date"), dict.get("start_time"), dict.get("end_time",""), dict.get("type","block"), dict.get("who",""), dict.get("what",""), dict.get("time",""), dict.get("location","")))
-
+        start_time = datetime.datetime.strptime(dict.get("start_time"), format)
+        end_time = datetime.datetime.strptime(dict.get("end_time"), format)
+        harmonogram.append(interval(start_time, end_time, dict.get("type","block"), dict.get("who",""), dict.get("what",""), dict.get("time",""), dict.get("location","")))
     return harmonogram
 
 def add_delay(harmonogram, minutes):
     for item in harmonogram:
         if not item.done:
             item.start_time + datetime.timedelta(minutes=minutes)
-            item.end_time + datetime.timedelta(minutes=minutes)
+            try:
+                item.end_time + datetime.timedelta(minutes=minutes)
+            except:
+                pass
+    return harmonogram
 
 
 tabulka = """| ŠTVRTOK 2.6. |                      |                                                 |
@@ -127,31 +166,9 @@ tabulka = """| ŠTVRTOK 2.6. |                      |                           
 | 16:00        | @everyone            | diskuse o sebahodnocení                         |
 
 """
-
 harmonogram = md2harmonogram(tabulka)
 save_harmonogram(harmonogram, harmonogram_path)
-# harmonogram = load_harmonogram(harmonogram_path)
-# for item in harmonogram:
-#     print(item)
+harmonogram = load_harmonogram(harmonogram_path)
 
-# bola_pauza = False
-# current_time = datetime.datetime.now()
-# for item in harmonogram:
-#     while not item.done:
-#         current_time = datetime.datetime.now()
-#         # if start_time uz bol, ale nie davnejseie ako 1 minutu dozadz, tak zvon
-#         if item.start_time <= current_time < item.start_time + datetime.timedelta(seconds=30):
-#             if item.type == "pause":
-#                 bola_pauza = True
-#                 playsound(sound_pause_start_path)
-#                 item.done = True
-#             elif bola_pauza == True:
-#                 bola_pauza = False
-#                 playsound(sound_pause_end_path)
-#                 item.done = True
-#             else :
-#                 playsound(sound_block_path)
-#                 item.done = True
-#         # ak start_time uz bol, davnejsie ako 1 minutut dozadu, tak proste done
-#         elif item.start_time < current_time:
-#             item.done = True
+bell = timer(harmonogram)
+bell.arm()
